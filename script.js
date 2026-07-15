06089ea770e1bd2e4d199a9c9e2fdb0cd35aac26
plac <bioxcthefirst@gmail.com>
2026-07-15 16:44:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const quotes = [
     { text: "The only way to do great work is to love what you do.", author: "Steve Jobs" },
+    { text: "Do not be afraid of an army of lions led by sheep. Be afraid of an army of sheep led by a lion.", author: "Alexander the Great" },
     { text: "Life is what happens when you're busy making other plans.", author: "John Lennon" },
     { text: "The future belongs to those who believe in the beauty of their dreams.", author: "Eleanor Roosevelt" },
     { text: "It is during our darkest moments that we must focus to see the light.", author: "Aristotle" },
